51a549038e1a206b25dcddd9322302598a1c2c5d
saveffer1 <saveffer01@gmail.com>
2023-10-08 22:11:21 +0700

    support swagger ui

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
+using NSwag;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddOpenApiDocument(options =>
+{
+    options.PostProcess = document =>
+    {
+        document.Info = new OpenApiInfo
+        {
+            Version = "v0.0.1",
+            Title = "Food-King Backend API",
+            Description = "An ASP.NET Web API for managing Food-King's backend services.",
+            // TermsOfService = "https://example.com/terms",
+            Contact = new OpenApiContact
+            {
+                Name = "Organization GitHub",
+                Url = "https://github.com/orgs/CE-WEBAPP-2023"
+            }
+            // License = new OpenApiLicense
+            // {
+            //     Name = "Example License",
+            //     Url = "https://example.com/license"
+            // }
+        };
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
 }
